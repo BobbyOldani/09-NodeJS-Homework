500470e4fbbffee1436401d486e7f1a35efe83d7
Noisey Turtle <bobbyoldani2@gmail.com>
2020-02-11 19:47:46 -0800

    generating html complete!

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-require("dotenv").config();
+const generateHTML = require("./generateHTML");
+// require("dotenv").config();
+let color
 
 const questions = [
     {
@@ -14,34 +16,62 @@ const questions = [
         type: 'list',
         message: 'Choose a color',
         name: 'color',
-        choices: ['green', 'red', 'orange', 'yellow', 'blue']
-
-
+        choices: ['green', 'blue', 'pink', 'red']
     }
 ];
 
+
+function init() {
 inquirer
     .prompt(questions)
-    .then(function({ username }) {
+    .then(function(responses) {
+        color = responses.color
+        const username = responses.username
         const queryUrl = `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
-        const newQueryUrl = `https://api.github.com/users/${username}/repos?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&per_page=100`
+        //const newQueryUrl = `https://api.github.com/users/${username}/repos?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&per_page=100`
     
         //  axios.get(queryUrl).then((res1) => {axios.get(newQueryUrl).then((res2.data) => {generateHTML(res) 
 
-        axios.get(queryUrl).then(function(res1){
-            console.log(res1.data);
+        axios.get(queryUrl) 
+        .then(function(res1){
+            const data = res1.data
+            console.log(data);
+             const user = {
+                 "avatar": data.avatar_url,
+                 "githubUrl": data.html_url,
+                 "name": data.name,
+                 "company": data.company, // possibly a function company() passing through data.company
+                 "blog": data.blog,
+                 "location": data.location,
+                 "bio": data.bio,
+                 "repos": data.public_repos,
+                 "following": data.following,
+                 "followers": data.followers,
+                 "color": color
+             }
+             console.log(user);
+             return generateHTML(user);
         })
+        .then(function(userReturn) {
+            //console.log(userReturn);
+            fs.writeFile("index.html", userReturn, function(err){
+                if(err) throw err;
+                console.log("WOHOOO!")
+            })
+        
+        })
+
+        
 
             ///
             /////.then (another api call for stars. Search into github api).
 
          
-            // })
+            
             // })
           });
-    
 
-
+ }
 
 
 // function writeToFile(fileName, data) {
@@ -50,10 +80,9 @@ inquirer
   
 // }
 
-// function init() {
 
+//  function company () {
+//      if 
+//  }
 
-
-// }
-
-// init();
+ init();
